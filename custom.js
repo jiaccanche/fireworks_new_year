7f2ccd60ce77e6198408ac30aec17372a7c23154
Jorge Canche <jcanchecauich@gmail.com>
2021-12-31 16:28:46 -0600

    clipboard

--- custom.js
+++ custom.js
@@ -1,35 +1,18 @@
-function createInputTxt(copyText){
-  console.log("sads",copyText);
-  return new Promise(resolve => {
-    console.log(copyText);
-    var dummy = document.createElement("input");
-
-    // Add it to the document
-    document.body.appendChild(dummy);
-    
-    // Set its ID
-    dummy.setAttribute("id", "dummy_id");
-    console.log(dummy);
-    // Output the array into it
-    document.getElementById("dummy_id").value=copyText;
-    document.getElementById("dummy_id").textContent = copyText;
-  
-    // Copy its contents
-    dummy.select();
-  });
- 
-}
-
-function copyClipboard(){
+function copyClipboard(normal){
   return new Promise(asd => {
-    res = document.execCommand("copy");
+    let text = document.getElementById("dummy_id");
+    console.log(text);   
+    text.focus();
+    text.select();
+    let res = document.execCommand("copy");
+    //res = document.execCommand("copy");
   if(res){
     console.log("bien");
     alert("URL copiada");
   }else{
     console.log("Ops! it didn't work");
     document.execCommand("copy");
-    alert("Es posible que no se hay hecho el copy");
+    alert("Ops! it didn't work");
   }
   });
   
@@ -39,13 +22,43 @@ function copyClipboard(){
  
 }
 
+function createInputTxt(copyText){
+  console.log("sads",copyText);
+  return new Promise(resolve => {
+    console.log(copyText);
+    var dummy = document.createElement("textarea");
+
+    // Add it to the document
+    document.body.appendChild(dummy);
+    
+    // Set its ID
+    dummy.setAttribute("id", "dummy_id");
+    console.log(dummy);
+    // Output the array into it
+    dummy.value=copyText;
+    dummy.textContent = copyText;
+    console.log(dummy);
+    let bton = document.createElement("button");
+    document.body.appendChild(bton);
+    bton.addEventListener('click', copyClipboard);
+    bton.click();
+    document.body.removeChild(bton);
+    bton.remove();
+
+
+  });
+ 
+}
+
+
+
 async function createInitials(copyText){
-  createInputTxt(copyText);
-  copyClipboard();
+  await createInputTxt(copyText);
+  //copyClipboard(copyText);
 }
 
 var changeName = function(){
-  var name = getUrlParam('name','A todos'); 
+  var name = getUrlParam('name','Sobreviviente'); 
   name = decodeURIComponent(name);
   console.log(name);
   document.getElementById("name").innerHTML = name;
@@ -59,8 +72,7 @@ var addPerson = function(){
     copyText = "User cancelled the prompt.";
     alert("Algo salio mal");
   } else {
-    copyText = "https://bit.ly/3eGUOxH?name=" + encodeURIComponent(person);
-    
+    copyText = "https://bit.ly/3eGUOxH?name=" + encodeURIComponent(person.length > 15 ? person.substring(0,14) : person );
     //console.log(copyText);
     createInitials(copyText);
     
@@ -86,6 +98,7 @@ function getUrlVars() {
 }
 
 function getUrlParam(parameter, defaultvalue){
+  console.log(parameter,defaultvalue );
   var urlparameter = defaultvalue;
   if(window.location.href.indexOf(parameter) > -1){
       urlparameter = getUrlVars()[parameter];
